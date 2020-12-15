76230d57aacb42f778fe615f8816f9f5d88f5486
rainbow23 <info@rainbow23.jp>
2020-12-16 00:45:05 +0900

    confirm config var value

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	// "bufio"
+	// "database/sql"
+	// "fmt"
+	// _ "github.com/go-sql-driver/mysql"
 	"io"
 	"net/http"
 	"os"
+	// "time"
 )
 
+var DBInfo struct {
+	host     string
+	Username string
+	Password string
+	Port     string
+	Database string
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
-	io.WriteString(w, os.Getenv("db_host"))
+	io.WriteString(w, "Hello World! \n")
+	io.WriteString(w, DBInfo.host)
 }
 
 func main() {
 	port := os.Getenv("PORT")
+	DBInfo.host = os.Getenv("db_host")
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":"+port, nil)
 }
