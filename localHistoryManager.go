41d24eee05d22000f7fa243061c9cac27c8efde8
rainbow23 <info@rainbow23.jp>
2020-12-27 00:58:46 +0900

    add local history process

--- localHistoryManager.go
+++ localHistoryManager.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -19,12 +22,6 @@ const (
 )
 
 type LocalHistoryInfo struct {
-	// HistoryList []struct {
-	//     timestamp int
-	//     nsec      int
-	//     command   string
-	// }
-
 	historyList []OneLineHistory
 }
 
@@ -36,25 +33,73 @@ type OneLineHistory struct {
 
 func fetchLocalHistory() {
 	fp, err := os.Open(HistoryFilePath)
+	defer fp.Close()
+
 	if err != nil {
 		panic(err)
 	}
-	defer fp.Close()
 
 	scanner := bufio.NewScanner(fp)
-	fmt.Println("-----------------------------------")
+
+	// fmt.Println("-----------------------------------")
+
 	buf := make([]byte, initialBufSize)
 	scanner.Buffer(buf, maxBufSize)
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-
-		// convertTimeStampToDate()
+		fmt.Println("-----------------------------------")
+		timeStamp, nSec, command := separateOneLine(scanner.Text())
+		fmt.Println(timeStamp)
+		fmt.Println(nSec)
+		fmt.Println(command)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("Scanner error: %q\n", err)
 	}
+}
+
+func separateOneLine(oneline string) (timeStamp int64, nSec int, command string) {
+	fmt.Println(oneline)
+	patternTimeStamp := `[0-9]{10}`
+	timeStampStr := cutOffOneline(patternTimeStamp, oneline)
+	timeStamp, _ = strconv.ParseInt(timeStampStr, 10, 64)
+
+	patternNsec := `:[0-9];+`
+	nSecStr := cutOffOneline(patternNsec, oneline)
+	nSecStr = strings.TrimLeft(nSecStr, ":")
+	nSecStr = strings.TrimRight(nSecStr, ";")
+	nSec, _ = strconv.Atoi(nSecStr)
+
+	patternCommand := "[0-9];.*$"
+	command = cutOffOneline(patternCommand, oneline)
+	command = command[2:]
+
+	return
+}
+
+func cutOffOneline(regexPattern string, material string) string {
+	// match, _ := regexp.MatchString(regexPattern, material)
+	regexp := regexp.MustCompile(regexPattern)
+	res := regexp.FindAllStringSubmatch(material, 1)
+	return res[0][0]
+}
+
+func test() {
+	oneline := "hoge:0045-111-2222 boke:0045-222-2222"
+	fmt.Println(oneline)
+
+	pattern := `[\d\-]+`
+	match, _ := regexp.MatchString(pattern, oneline)
+	fmt.Println(match)
+
+	rep := regexp.MustCompile(pattern)
+
+	result := rep.Split(oneline, 1)
+	fmt.Println(result[0])
+
+	r := regexp.MustCompile(pattern)
+	fmt.Println(r.FindAllStringSubmatch(oneline, -1)) // => "[[0045-111-2222] [0045-222-2222]]"
 }
 
 func convertTimeStampToDate(timeStamp int64, nsec int64) string {
