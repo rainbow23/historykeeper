9f9228a792f0433cefe24adfe35f1e8220484e39
rainbow23 <info@rainbow23.jp>
2020-12-31 02:32:48 +0900

    add register user

--- sqlManager/insertData.go
+++ sqlManager/insertData.go
@@ -9,14 +9,54 @@ import (
 )
 
 const (
-	database = "shell_history2"
+	tableHistory  = "shell_history2"
+	tableUserInfo = "user_info"
 )
 
 func InsertHistory(username string, linesHistory localHistory.OneLineHistory) {
 	query := fmt.Sprintf("INSERT INTO %s(%s, %s, %s) VALUES(?,?,?)",
-		database, "username", "command", "date")
+		tableHistory, "username", "command", "date")
 
 	insertTemplate(query, username, linesHistory.Command, linesHistory.Date)
+}
+
+func RegisterUser(username string, password string) {
+	if username == "" || password == "" {
+		return
+	}
+
+	query := fmt.Sprintf("INSERT INTO %s(%s, %s) VALUES(?,?)",
+		tableUserInfo, "username", "password")
+
+	register(query, username, password)
+}
+
+func register(query string, username string, password string) {
+	if username == "" || password == "" {
+		return
+	}
+
+	Db := sqlConnect()
+	defer Db.Close()
+
+	//dbの最新保存日付より新しい日付のローカル履歴があれば保存する
+
+	stmtIns, err := Db.Prepare(query) // ? = placeholder
+
+	if err != nil {
+		fmt.Println("err first")
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
+
+	//後にcommand, date 抽象化する
+	_, err = stmtIns.Exec(username, password) // Insert tuples (i, i^2)
+
+	if err != nil {
+		fmt.Println("err second")
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
 }
 
 func insertTemplate(query string, username string, command string, date string) {
