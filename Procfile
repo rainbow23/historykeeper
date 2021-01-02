92adc6c74c100410645e66b846c0540915405cbf
rainbow23 <info@rainbow23.jp>
2021-01-02 23:08:59 +0900

    add x flag to shell file

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: chmod u+x shell/uuid.zsh
 web: bin/historyKeeper
--- utils/utils.go
+++ utils/utils.go
@@ -10,7 +10,7 @@ func FetchUUID() string {
 	// コマンドを実行 .(ピリオド)は使えずにちゃんと sh コマンドを使う
 	out, err := exec.Command(os.Getenv("SHELL"), "shell/uuid.zsh").Output()
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("utils " + err.Error())
 		os.Exit(1)
 	}
 	// fmt.Println(string(out))
